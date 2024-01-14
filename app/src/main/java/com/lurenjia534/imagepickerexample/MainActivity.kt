cce42a2df5ce52f1cff55d2f4b396896d23df4cd
lurenjia534 <lurenjia534@outlook.com>
2024-01-14 14:09:43 +0800

    App: Screenshot detection

--- app/src/main/java/com/lurenjia534/imagepickerexample/MainActivity.kt
+++ app/src/main/java/com/lurenjia534/imagepickerexample/MainActivity.kt
@@ -1,46 +1,42 @@
 package com.lurenjia534.imagepickerexample
 
-import android.graphics.drawable.Icon
-import android.net.Uri
+import android.app.Activity
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Share
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.lurenjia534.imagepickerexample.ui.theme.ImagePickerExampleTheme
-import java.time.format.TextStyle
 
 class MainActivity : ComponentActivity() {
+    private val ss = Activity.ScreenCaptureCallback{
+
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -50,10 +46,26 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.primary
                 ) {
+
+                 //   registerScreenCaptureCallback(mainExecutor, screenCaptureCallback)
                     ImagePickerExample()
                 }
             }
         }
+    }
+    // @RequiresApi(Build.VERSION_CODES.UPSIDE_DOWN_CAKE)
+    @RequiresApi(Build.VERSION_CODES.UPSIDE_DOWN_CAKE)
+    override fun onStart() {
+        super.onStart()
+        // 注册屏幕截图检测回调
+        registerScreenCaptureCallback(mainExecutor, ss)
+    }
+    // @RequiresApi(Build.VERSION_CODES.UPSIDE_DOWN_CAKE)
+    @RequiresApi(Build.VERSION_CODES.UPSIDE_DOWN_CAKE)
+    override fun onStop() {
+        super.onStop()
+        // 注销屏幕截图检测回调
+        unregisterScreenCaptureCallback(ss)
     }
 }
 
